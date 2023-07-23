fa75fbf14416f148542415a82190acb4d23022c0
pachuboing747 <pachuboing747@hotmail.com>
2023-07-23 09:59:55 -0300

    s

--- routes/api/Cart-router.js
+++ routes/api/Cart-router.js
@@ -32,54 +32,6 @@ router.post("/", async (req, res) => {
   res.status(201).send(cart);
 });
 
-// router.post("/:cid/product/:pid", async (req, res) => {
-//   try {
-//     const cid = parseInt(req.params.cid);
-//     const pid = parseInt(req.params.pid);
-//     const cantidad = 1; 
-
-//     const cart = await cartManager.getById(cid);
-
-//     if (!cart) {
-//       res.status(404).send("No se encuentra un carrito de compras con el identificador proporcionado");
-//       return;
-//     }
-
-//     const product = await productManager.getById(pid);
-
-//     if (product) {
-//       res.send("producto encontrado");
-//       return;
-//     } else if (product.cantidad === 0) {
-//       res.status(201).send("error");
-//       return;
-//     }
-
-//     const existe = await cartManager.existInCart(cart, pid);
-
-//     if (existe === undefined) {
-//       // Crear un nuevo producto en el carrito
-//       const productInCart = await cartManager.createProduct(cart, pid, cantidad);
-
-//       res.status(201).send(`El producto con ID ${productInCart.products[0].product} ha sido ingresado correctamente y su cantidad es ${productInCart.products[0].quantity}`);
-//     } else {
-//       if (existe.quantity === product.stock) {
-//         res.status(201).send("No contamos con stock para el producto solicitado, disculpe las molestias");
-//       } else {
-//         // Actualizar la cantidad del producto existente en el carrito
-//         const productInCart = await cartManager.updateProduct(cart, pid);
-
-//         res.status(201).send(`El producto con ID ${pid} ha sido actualizado correctamente y su cantidad es ${productInCart}`);
-//       }
-//     }
-//   } catch (e) {
-//     res.status(500).send({
-//       message: "Ha ocurrido un error en el servidor",
-//       exception: e.stack,
-//     });
-//   }
-// });
-
 router.post('/:cid/product/:pid', async (req, res) => {
   const cid = req.params.cid;
   const pid = req.params.pid;
