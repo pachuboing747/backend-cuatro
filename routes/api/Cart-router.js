2fb0f85621047c52eed8069e9cbfa3b542e776a4
pachuboing747 <pachuboing747@hotmail.com>
2023-07-23 07:32:32 -0300

    cart.json

--- routes/api/Cart-router.js
+++ routes/api/Cart-router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import CartsManager from "../../managers/CartsManager.js";
 import ProductManager from "../../managers/ProductManager.js";
-
+import fs from "fs/promises";
 
 const cartManager = new CartsManager("cart.json");
 const productManager = new ProductManager("productos.json");
@@ -32,53 +32,95 @@ router.post("/", async (req, res) => {
   res.status(201).send(cart);
 });
 
-router.post("/:cid/product/:pid", async (req, res) => {
+// router.post("/:cid/product/:pid", async (req, res) => {
+//   try {
+//     const cid = parseInt(req.params.cid);
+//     const pid = parseInt(req.params.pid);
+//     const cantidad = 1; 
+
+//     const cart = await cartManager.getById(cid);
+
+//     if (!cart) {
+//       res.status(404).send("No se encuentra un carrito de compras con el identificador proporcionado");
+//       return;
+//     }
+
+//     const product = await productManager.getById(pid);
+
+//     if (product) {
+//       res.send("producto encontrado");
+//       return;
+//     } else if (product.cantidad === 0) {
+//       res.status(201).send("error");
+//       return;
+//     }
+
+//     const existe = await cartManager.existInCart(cart, pid);
+
+//     if (existe === undefined) {
+//       // Crear un nuevo producto en el carrito
+//       const productInCart = await cartManager.createProduct(cart, pid, cantidad);
+
+//       res.status(201).send(`El producto con ID ${productInCart.products[0].product} ha sido ingresado correctamente y su cantidad es ${productInCart.products[0].quantity}`);
+//     } else {
+//       if (existe.quantity === product.stock) {
+//         res.status(201).send("No contamos con stock para el producto solicitado, disculpe las molestias");
+//       } else {
+//         // Actualizar la cantidad del producto existente en el carrito
+//         const productInCart = await cartManager.updateProduct(cart, pid);
+
+//         res.status(201).send(`El producto con ID ${pid} ha sido actualizado correctamente y su cantidad es ${productInCart}`);
+//       }
+//     }
+//   } catch (e) {
+//     res.status(500).send({
+//       message: "Ha ocurrido un error en el servidor",
+//       exception: e.stack,
+//     });
+//   }
+// });
+
+router.post('/:cid/product/:pid', async (req, res) => {
+  const cid = req.params.cid;
+  const pid = req.params.pid;
+
   try {
-    const cid = parseInt(req.params.cid);
-    const pid = parseInt(req.params.pid);
-    const cantidad = 1; 
+    let data = await fs.readFile('./data/cart.json', 'utf-8');
+    let cartList = JSON.parse(data);
 
-    const cart = await cartManager.getById(cid);
+    const cartIndex = cartList.findIndex((item) => item.cid === cid);
 
-    if (!cart) {
-      res.status(404).send("No se encuentra un carrito de compras con el identificador proporcionado");
-      return;
-    }
+    if (cartIndex !== -1) {
+      const cart = cartList[cartIndex];
+      const existingProductIndex = cart.products.findIndex((item) => item.product === pid);
 
-    const product = await productManager.getById(pid);
-
-    if (product) {
-      res.send("producto encontrado");
-      return;
-    } else if (product.cantidad === 0) {
-      res.status(201).send("error");
-      return;
-    }
-
-    const existe = await cartManager.existInCart(cart, pid);
-
-    if (existe === undefined) {
-      // Crear un nuevo producto en el carrito
-      const productInCart = await cartManager.createProduct(cart, pid, cantidad);
-
-      res.status(201).send(`El producto con ID ${productInCart.products[0].product} ha sido ingresado correctamente y su cantidad es ${productInCart.products[0].quantity}`);
-    } else {
-      if (existe.quantity === product.stock) {
-        res.status(201).send("No contamos con stock para el producto solicitado, disculpe las molestias");
+      if (existingProductIndex !== -1) {
+        // If the product already exists in the cart, increase its quantity
+        cart.products[existingProductIndex].quantity++;
       } else {
-        // Actualizar la cantidad del producto existente en el carrito
-        const productInCart = await cartManager.updateProduct(cart, pid);
-
-        res.status(201).send(`El producto con ID ${pid} ha sido actualizado correctamente y su cantidad es ${productInCart}`);
+        // If the product doesn't exist, add it to the cart
+        const newProductId = cart.products.length + 1;
+        const newProduct = { id: newProductId, product: pid, quantity: 1 };
+        cart.products.push(newProduct);
       }
+    } else {
+      // If the cart doesn't exist, create a new cart and add the product to it
+      const newProduct = { id: 1, product: pid, quantity: 1 };
+      const newCart = { cid, products: [newProduct] };
+      cartList.push(newCart);
     }
-  } catch (e) {
-    res.status(500).send({
-      message: "Ha ocurrido un error en el servidor",
-      exception: e.stack,
-    });
+
+    data = JSON.stringify(cartList, null, 2);
+    await fs.writeFile('./data/cart.json', data, 'utf-8');
+
+    res.json(cartList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al agregar el producto al carrito' });
   }
 });
+
+
 
 
 export default router;
